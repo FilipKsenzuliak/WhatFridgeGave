59d4dc7972bdab84108fbab0fa8b3b7465ec5f16
unknown <filipksenzuliak@gmail.com>
2017-11-19 23:11:03 +0100

    [CREATE] initial repository

---
+++ WhatFridgeGave/ClientApp/app/components/mycomp/mycomp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mycomp',
+    templateUrl: './mycomp.component.html'
+})
+export class MycompComponent {
+    public hello = 'caaaves;'
+}
